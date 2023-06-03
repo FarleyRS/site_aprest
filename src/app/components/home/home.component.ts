d4f965935d450221cf5edf8d7f1fe36f02b34dae
FarleyRS <f7739609@gmail.com>
2023-06-03 12:04:56 -0300

    atualização 2.9

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  showButton = false;
+  
+  constructor(private router: Router) { }
+  
+  ngOnInit() {
+    setTimeout(() => {
+      this.showButton = true;   
+    }, 7000); 
+  }
+
+  onActivateVibration() {
+    // Define o atraso de 2000 milissegundos (2 segundos)
+    navigator.vibrate([500, 200, 800, 200, 800]);
+    this.router.navigate(['form']);
+    console.log("Procurando algo?");
+  }
 
 }
